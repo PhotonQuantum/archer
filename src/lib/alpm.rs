7e93fc59b85d94675d288e40bf16586fcd200147
PhotonQuantum <self@lightquantum.me>
2021-05-10 02:45:46 +0800

    refactor: remove unused AlpmBuilder

--- src/lib/alpm.rs
+++ src/lib/alpm.rs
@@ -4,9 +4,7 @@ use alpm::Alpm;
 use lazy_static::lazy_static;
 
 use crate::consts::*;
-use crate::error::Result;
-use crate::parser::pacman::SyncDB;
-use crate::parser::{PacmanParser, GLOBAL_CONFIG};
+use crate::parser::GLOBAL_CONFIG;
 
 lazy_static! {
     pub static ref GLOBAL_ALPM: Arc<Mutex<alpm::Alpm>> = {
@@ -17,29 +15,4 @@ lazy_static! {
         }
         Arc::new(Mutex::new(alpm))
     };
-}
-
-#[derive(Clone)]
-pub struct AlpmBuilder {
-    sync_dbs: Vec<SyncDB>,
-}
-
-impl AlpmBuilder {
-    pub fn new(config: &PacmanParser) -> Self {
-        Self {
-            sync_dbs: config.sync_dbs(),
-        }
-    }
-
-    pub fn build(&self) -> Result<Alpm> {
-        Ok(Alpm::new(ROOT_PATH, PACMAN_DB_PATH)?)
-    }
-
-    pub fn build_sync(&self) -> Result<Alpm> {
-        let alpm = Alpm::new(ROOT_PATH, PACMAN_DB_PATH)?;
-        for db in &self.sync_dbs {
-            alpm.register_syncdb(db.name.to_string(), db.sig_level)?;
-        }
-        Ok(alpm)
-    }
 }
